379030b9d46b2c74cd565c5d8914415c39644a61
Gabriel Brandersky <gabo6661@gmail.com>
2013-12-06 15:15:31 +0100

    Add show testing

---
+++ spec/integration/basic/show/lepidlo_basic_show_spec.rb
@@ -0,0 +1,177 @@
+require 'spec_helper'
+
+describe "Lepidlo Basic Show" do
+  subject { page }
+
+  describe "GET /employees/1" do
+    before do
+      @employee = FactoryGirl.create :employee
+    end
+
+    it "has Edit, Delete and attributes" do
+      visit employee_path(:id => @employee.id)
+      expect(page).to have_selector("a", :text => "Edit")
+      expect(page).to have_selector("a", :text => "Delete")
+      
+      Employee.attribute_names.each do |attr|
+        next if attr.in?(["id", "created_at", "updated_at"])
+        expect(page).to have_content(Employee.human_attribute_name(attr))
+        expect(page).to have_content(@employee.send(attr))
+      end
+    end
+
+    it "shows only defined columns" do
+      RailsAdmin.config Employee do
+        show do
+          field :name
+          field :income
+        end
+      end
+
+      visit employee_path(:id => @employee.id)
+      
+      # TODO should I test every attribute? I did not 
+      # because I would have to set them while creating factory
+      expect(page).to have_content(@employee.name)
+      expect(page).to have_content(@employee.income)
+
+      expect(page).to have_no_content(@employee.email)
+    end
+
+    it "properly format date columns" do
+      RailsAdmin.config Employee do
+        show do
+          field :name
+          field :created_at
+        end
+      end
+
+      visit employee_path(:id => @employee.id)
+
+      expect(page).to have_content(I18n.l @employee.created_at, format: :long)
+    end
+
+    it "properly shows boolean field type" do
+      pending "add some examples to (or delete) #{__FILE__}"
+    end
+
+    it "properly shows belongs_to association" do
+      RailsAdmin.config Employee do
+        show do
+          field :position
+        end
+      end
+      
+      @employee.position = FactoryGirl.create(:position, name: "My Position")
+      @employee.save!
+      
+      visit employee_path(:id => @employee.id)
+      expect(page).to have_content('My Position')
+
+
+      #TODO: rozdelit na dva testcasy - jeden se zakazanym ablity na cannot :show, Position (ten to zobrazi pouze jako text) a druhy s povoleny, pro ten to bude link
+      #should_not have_selector(:link_or_button, 'My Position')
+    end
+
+
+    # it "respons with :json" do
+    #   visit employees_path(:format => :json)
+    #   expect(ActiveSupport::JSON.decode(page.body).length).to eq(2)
+    #   ActiveSupport::JSON.decode(page.body).each do |object|
+    #     expect(object).to have_key("id")
+    #     expect(object).to have_key("name")
+    #     expect(object).to have_key("email")
+    #     expect(object).to have_key("created_at")
+    #     expect(object).to have_key("updated_at")
+    #   end
+    # end
+
+    # it "respons with :xml" do
+    #   pending "add some examples to (or delete) #{__FILE__}"
+    # end
+
+  end
+
+  describe "show with has-and-belongs-to-many association" do
+    before(:each) do
+      @employee = FactoryGirl.create :employee
+      @task1 = FactoryGirl.create :task, :employee_id => @employee.id, description: "first task"
+      @task2 = FactoryGirl.create :task, :employee_id => @employee.id, description: "second task"
+
+      RailsAdmin.config Employee do
+        field :tasks
+      end
+      visit employee_path(:id => @employee.id)
+    end
+
+    # TODO withouth access not as links
+    it "shows associated objects" do      
+      expect(page).to have_content('first task and second task')
+      expect(page).to have_selector(:link_or_button, 'first task')
+      expect(page).to have_selector(:link_or_button, 'second task')
+    end
+  end
+
+=begin
+  describe "GET /admin/employees/123this-id-doesnt-exist" do
+    it "raises NotFound" do
+      visit '/admin/employees/123this-id-doesnt-exist'
+      expect(page.driver.status_code).to eq(404)
+    end
+  end
+
+  describe "show with belongs_to association" do
+    before(:each) do
+      @employee = FactoryGirl.create :employee
+      @team   = FactoryGirl.create :team
+      @employee.update_attributes(:team_id => @team.id)
+      visit employee_path(:id => @employee.id)
+    end
+
+    it "shows associated objects" do
+      expect(page).to have_css("a[href='/admin/team/#{@team.id}']")
+    end
+  end
+
+  describe "show with has-one association" do
+    before(:each) do
+      @employee = FactoryGirl.create :employee
+      @draft  = FactoryGirl.create :draft, :employee => @employee
+      visit employee_path(:id => @employee.id)
+    end
+
+    it "shows associated objects" do
+      should have_css("a[href='/admin/draft/#{@draft.id}']")
+    end
+  end
+
+  describe "show with has-and-belongs-to-many association" do
+    before(:each) do
+      @employee = FactoryGirl.create :employee
+      @comment1 = FactoryGirl.create :comment, :commentable => @employee
+      @comment2 = FactoryGirl.create :comment, :commentable => @employee
+      @comment3 = FactoryGirl.create :comment, :commentable => FactoryGirl.create(:employee)
+
+      visit employee_path(:id => @employee.id)
+    end
+
+    it "shows associated objects" do
+      should have_css("a[href='/admin/comment/#{@comment1.id}']")
+      should have_css("a[href='/admin/comment/#{@comment2.id}']")
+      should_not have_css("a[href='/admin/comment/#{@comment3.id}']")
+    end
+  end
+
+  describe "show for polymorphic objects" do
+    before(:each) do
+      @employee = FactoryGirl.create :employee
+      @comment = FactoryGirl.create :comment, :commentable => @employee
+      visit employee_path(:model_name => "comment", :id => @comment.id)
+    end
+
+    it "shows associated object" do
+      should have_css("a[href='/admin/employee/#{@employee.id}']")
+    end
+  end
+=end
+end
